7c17f42bda2683ba2d730d048f554412304f981d
bharat1987 <bharatpitta9@gmail.com>
2020-05-27 12:21:45 +0530

    removing infosys folder

--- AccountMS/src/main/java/com/infosys/msproject/exception/UserIdAlreadyPresentException.java
+++
@@ -1,8 +0,0 @@
-package com.infosys.msproject.exception;
-
-
-public class UserIdAlreadyPresentException extends Exception {
-	public UserIdAlreadyPresentException(String message){
-		super(message);
-	}
-}
--- AccountMS/src/main/java/com/infosys/msproject/repository/AccountRepository.java
+++
@@ -1,9 +0,0 @@
-package com.infosys.msproject.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.infosys.msproject.entity.UserInfo;
-
-public interface AccountRepository extends JpaRepository<UserInfo, String> {
-
-}
